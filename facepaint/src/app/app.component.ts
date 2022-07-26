b21d5b57184402d2ef9a427be6b9fb2b0c08d8ba
gbdev25 <gbdev25@gmail.com>
2022-07-26 09:05:26 -0400

    finished create user functionality

--- facepaint/src/app/app.component.ts
+++ facepaint/src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-import { userInt } from './get-user/userInt';
 
 @Component({
   selector: 'app-root',
--- facepaint/src/app/services/user.service.ts
+++ facepaint/src/app/services/user.service.ts
@@ -1,15 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
 
-import { TitleStrategy } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  
+  // constructor injection
+  constructor(private http: HttpClient,) { }
 
-  constructor() { }
+  getUsers(): Observable<User[]> {
+    return this.http.get(`${environment.apiUrl}/users`, {
+      headers: {
 
+      }
+    }).pipe(
+      map(
+        response => response as User[]
+      )
+    );
+  }
+
+  createUser(newUser: User): Observable<User> {
+    return this.http.post(`${environment.apiUrl}/users`, newUser, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    ).pipe(
+      map(
+        response => response as User
+      )
+    );
+  }
 }
-
