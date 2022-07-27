176ab35569359d5a70dbdf02a87dc5b5d6d6fc63
Ashley Kang <ashleykang99@gmail.com>
2022-07-27 14:38:42 -0400

    test function

--- facepaint/src/app/components/art/art.component.ts
+++ facepaint/src/app/components/art/art.component.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import {DomSanitizer} from '@angular/platform-browser';
+import { Art } from 'src/app/models/art';
 
 @Component({
   selector: 'app-art',
@@ -46,7 +47,33 @@ getImageFromService() {
     this.createImageFromBlob(data);
   });
 }
+
+newData: any;
+data: any;
+artInfoforShow:any;
+
+getArtInfo(){
+  let queryParam = new HttpParams().set('artID', '179972');
+  return this.http.get(this.ROOT_URL + '/artInfo', {params:queryParam});
+  //console.group(this.data);
 }
 
+showArtInfo(){
+  this.getArtInfo().subscribe(val => 
+    this.data = val
+);
+  console.log(this.data);
+  //console.log(this.data[0].image_id);
+  //this.artInfoforShow = new Art(this.data.image_id,this.data.title,this.data.artist_display);
+  //console.log(JSON.stringify(this.data).substring(0,20));
+  //var toshow = JSON.stringify(this.data);
+  //console.log(toshow.substring(1, 10));
+   //console.log(this.newData);
+  //console.log(this.data.json());
+  //console.log(this.data);
+  //this.getArtInfo().subscribe((res) => {
+  //this.data = JSON.parse(res.toString());
+  //});
   
-//2fa24f36-cc26-41b6-4b49-12bba2a6c1c8
+}
+}
