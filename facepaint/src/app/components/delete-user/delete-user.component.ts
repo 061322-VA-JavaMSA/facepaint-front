3a9269d707bc8f0fbd837b863dee5b28f7624850
gbdev25 <gbdev25@gmail.com>
2022-07-28 17:02:54 -0400

    changes

--- facepaint/src/app/components/delete-user/delete-user.component.ts
+++ facepaint/src/app/components/delete-user/delete-user.component.ts
@@ -28,7 +28,7 @@ export class DeleteUserComponent implements OnInit {
 
     this.userServ.deleteUser(remUser).subscribe(
       () => {
-        this.router.navigate(['']);
+        this.router.navigate(['getusers']);
       },
       () => {
         this.errorMessage = 'User does not exist'
