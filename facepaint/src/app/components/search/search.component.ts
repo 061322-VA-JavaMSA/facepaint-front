baaeed3e19f0363a22fc489bc669fb1a5fa0ffac
Ashley Kang <ashleykang99@gmail.com>
2022-07-29 12:30:39 -0400

    added view details functionality. Added buttons. Set up for updating Showcase art

--- facepaint/src/app/components/search/search.component.ts
+++ facepaint/src/app/components/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ArtRetrievalService } from 'src/app/services/art-retrieval.service';
 
 @Component({
@@ -12,10 +12,11 @@ export class SearchComponent implements OnInit {
   searchTitles!: string[];
   searchArtists!: string[];
   searchImages: any[];
+  searchIDS : string[];
   searchParam: string;
   isFilled = false;
 
-  constructor(private artServ: ArtRetrievalService, private activated:ActivatedRoute) { }
+  constructor(private artServ: ArtRetrievalService, private activated:ActivatedRoute, private router:Router) { }
 
   ngOnInit(): void {
     this.activated.paramMap.subscribe( paramMap => {
@@ -37,6 +38,12 @@ export class SearchComponent implements OnInit {
     this.searchTitles = this.artServ.searchArrTitles;
     this.searchArtists = this.artServ.searchArrArtists;
     this.searchImages = this.artServ.imageURL;
+    this.searchIDS = this.artServ.returnArtIDS;
     this.isFilled = true;
+  }
+
+
+  goToDetails(artID: string){
+    this.router.navigate(['art', {p1:artID}]);
   }
 }
