be93855cafb668928dd2c557c023d441b2ac7157
gbdev25 <gbdev25@gmail.com>
2022-07-29 15:07:03 -0400

    refresh change wip

--- facepaint/src/app/components/delete-user/delete-user.component.ts
+++ facepaint/src/app/components/delete-user/delete-user.component.ts
@@ -29,7 +29,7 @@ export class DeleteUserComponent implements OnInit {
     this.userServ.deleteUser(remUser).subscribe(
       () => {
         this.router.navigate(['getusers']);
-        window.location.reload();
+        this.ngOnInit();
       },
       () => {
         this.errorMessage = 'User does not exist'
